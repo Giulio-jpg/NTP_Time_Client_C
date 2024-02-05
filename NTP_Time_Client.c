d97e7b43278ae72bf6fd68611d2a6e37f7688594
GiulioLV98 <giulioleoni5@gmail.com>
2024-02-05 16:09:34 +0100

    Update NTP_Time_Client.c

--- NTP_Time_Client.c
+++ NTP_Time_Client.c
@@ -11,20 +11,20 @@
 #include <stdio.h>
 #define SECONDS_IN_70_YEARS 2208988800
 
-typedef struct ntp_packet
+typedef struct ntpPacket
 {
-    unsigned char leap_version_mode;
+    unsigned char leapVersionMode;
     unsigned char stratum;
     unsigned char poll;
     unsigned char precision;
-    unsigned long root_delay;
-    unsigned long root_dispersion;
+    unsigned long rootDelay;
+    unsigned long rootDispersion;
     char rfid[4];
-    unsigned long long ref_timestamp;
-    unsigned long long original_timestamp;
-    unsigned long long receive_timestamp;
-    unsigned long long transmit_timestamp;
-} ntp_packet;
+    unsigned long long refTimestamp;
+    unsigned long long originalTimestamp;
+    unsigned long long receiveTimestamp;
+    unsigned long long transmitTimestamp;
+} ntpPacket;
 
 
 
@@ -32,8 +32,8 @@ int main(int argc, char **argv)
 {
 
 #ifdef _WIN32
-    WSADATA wsa_data;
-    if (WSAStartup(0x0202, &wsa_data))
+    WSADATA wsaData;
+    if (WSAStartup(0x0202, &wsaData))
     {
         printf("unable to initialize winsock2 \n");
         return -1;
@@ -60,50 +60,49 @@ int main(int argc, char **argv)
 
     time_t now = time(NULL);
 
-    ntp_packet packet = {leap << 6 | version << 3 | mode, 0, 0, 0, 0, 0, {0, 0, 0, 0}, 0, now, 0, 0};
+    ntpPacket packet = {leap << 6 | version << 3 | mode, 0, 0, 0, 0, 0, {0, 0, 0, 0}, 0, now, 0, 0};
 
-    int sent_bytes = sendto(s, (char *)&packet, sizeof(packet), 0, (struct sockaddr *)&sin, sizeof(sin));
-    printf("sent %d bytes via UDP \n", sent_bytes);
+    int sentBytes = sendto(s, (char *)&packet, sizeof(packet), 0, (struct sockaddr *)&sin, sizeof(sin));
+    printf("sent %d bytes via UDP \n", sentBytes);
 
     for (;;)
     {
         struct sockaddr_in sender_in;
-        int sender_in_size = sizeof(sender_in);
-        int len = recvfrom(s, (char *)&packet, sizeof(packet), 0, (struct sockaddr *)&sender_in, &sender_in_size);
+        int senderInSize = sizeof(sender_in);
+        int len = recvfrom(s, (char *)&packet, sizeof(packet), 0, (struct sockaddr *)&sender_in, &senderInSize);
         if (len > 0)
         {
-            char addr_as_string[64];
-            inet_ntop(AF_INET, &sender_in.sin_addr, addr_as_string, 64);
-            printf("received %d bytes from %s:%d\n", len, addr_as_string, ntohs(sender_in.sin_port));
+            char addrAsString[64];
+            inet_ntop(AF_INET, &sender_in.sin_addr, addrAsString, 64);
+            printf("received %d bytes from %s:%d\n", len, addrAsString, ntohs(sender_in.sin_port));
 
             // converting from network byte order (which is big endian) to host byte order (which, usually, is little endian)
-            packet.root_delay = ntohl(packet.root_delay);
-            packet.root_dispersion = ntohl(packet.root_dispersion);
+            packet.rootDelay = ntohl(packet.rootDelay);
+            packet.rootDispersion = ntohl(packet.rootDispersion);
 
 
 
-            time_t actual_time = ntohl(packet.receive_timestamp);
-            packet.ref_timestamp = ntohll(packet.ref_timestamp);
-            packet.original_timestamp = ntohll(packet.original_timestamp);
-            packet.receive_timestamp = ntohll(packet.receive_timestamp);
-            packet.transmit_timestamp = ntohll(packet.transmit_timestamp);
+            time_t actualTime = ntohl(packet.receiveTimestamp);
+            packet.refTimestamp = ntohll(packet.refTimestamp);
+            packet.originalTimestamp = ntohll(packet.originalTimestamp);
+            packet.receiveTimestamp = ntohll(packet.receiveTimestamp);
+            packet.transmitTimestamp = ntohll(packet.transmitTimestamp);
 
-            printf("Leap second: %d \nVersion: %d \nMode: %d\n", packet.leap_version_mode >> 6, (packet.leap_version_mode >> 3) & 0b111, packet.leap_version_mode & 0b111);
+            printf("Leap second: %d \nVersion: %d \nMode: %d\n", packet.leapVersionMode >> 6, (packet.leapVersionMode >> 3) & 0b111, packet.leapVersionMode & 0b111);
             printf("Stratum: %d \nPoll: %d \nPrecision: %d\n", packet.stratum, packet.poll, packet.precision);
-            printf("Root delay: %lu\n", packet.root_delay);
-            printf("Root dispersion: %lu\n", packet.root_dispersion);
+            printf("Root delay: %lu\n", packet.rootDelay);
+            printf("Root dispersion: %lu\n", packet.rootDispersion);
             printf("Ref ID: %c%c%c%c\n", packet.rfid[0], packet.rfid[1], packet.rfid[2], packet.rfid[3]);
-            // Here I'm cutting the fractionary part, to show only the seconds passed from 1/01/1900
-            printf("Ref timestamp: %llu\n",      packet.ref_timestamp);
-            printf("Original timestamp: %llu\n", packet.original_timestamp);
-            printf("Received timestamp: %llu\n", packet.receive_timestamp);
-            printf("Transmit timestamp: %llu\n", packet.transmit_timestamp);
+            printf("Ref timestamp: %llu\n",      packet.refTimestamp);
+            printf("Original timestamp: %llu\n", packet.originalTimestamp);
+            printf("Received timestamp: %llu\n", packet.receiveTimestamp);
+            printf("Transmit timestamp: %llu\n", packet.transmitTimestamp);
 
-            actual_time -= SECONDS_IN_70_YEARS;
+            actualTime -= SECONDS_IN_70_YEARS;
 
             char outstr[4096];
             struct tm* tmp;
-            tmp = localtime(&actual_time);
+            tmp = localtime(&actualTime);
             strftime(outstr, sizeof(outstr), "%A, %d %B %Y %T", tmp);
             printf("Result string is \"%s\"\n", outstr);
 
